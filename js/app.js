1b9d34a0ea8c23f6bf8bdf12f021c3eb32218088
Kevin Suarez <kdock994@gmail.com>
2020-02-10 19:43:31 -0500

    font changes

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
-//////////////////// CONSTANTS /////////////////////////////////////
-
+///////////////////// CONSTANTS /////////////////////////////////////
 const winningConditions = [
   [0, 1, 2],
   [3, 4, 5],
@@ -12,58 +11,66 @@ const winningConditions = [
 ];
 
 ///////////////////// APP STATE (VARIABLES) /////////////////////////
-
 let board;
 let turn;
 let win;
-let x_wins_count = 0
-let o_wins_count = 0
-let switch_turn_count = 0
-///////////////////// CACHED ELEMENT REFERENCES /////////////////////
+let determine_first_player;
+let x_wins = 0;
+let o_wins = 0;
+let ties= 0
 
+///////////////////// CACHED ELEMENT REFERENCES /////////////////////
 const squares = Array.from(document.querySelectorAll("#board div"));
 const message = document.querySelector("h2");
 
 ///////////////////// EVENT LISTENERS ///////////////////////////////
-
 window.onload = init;
-
 document.getElementById("board").onclick = takeTurn;
 document.getElementById("reset-button").onclick = init;
-document.getElementById("switch").onclick = switch_turn;
+document.getElementById("reset-scoreboard").onclick = resetScoreboard;
 ///////////////////// FUNCTIONS /////////////////////////////////////
 function init() {
-  board = ["", "", "", "", "", "", "", "", ""];
-  if (switch_turn_count == 0) {
-    turn = "X";
-  }
-  else if (switch_turn_count == 1) {
-    turn = "O"
-  }
+  board = [
+    "", "", "",
+    "", "", "",
+    "", "", ""
+  ];
+
+  do {
+    var first_Player = prompt("Enter X or O to determine who goes first: ");
+    if (first_Player === null) {
+      turn = "X";
+      break;
+    } else if (first_Player === "X" || first_Player === "x") {
+      turn = "X";
+    } else if (first_Player === "O" || first_Player === "o") {
+      turn = "O";
+    } else {
+      determine_first_player = L;
+    }
+  } while (first_Player !== "X" && first_Player !== "x" && first_Player !== "O" && first_Player !== "o");
+
   win = null;
 
   render();
 }
-function switch_turn() {
-  if (switch_turn_count == 0) {
-    switch_turn_count = 1
-  }
-  else if (switch_turn_count == 1) {
-    switch_turn_count = 0
-  }
-}
+
 function render() {
   board.forEach(function(mark, index) {
     squares[index].textContent = mark;
   });
   if (win === "X") {
-    x_wins_count = x_wins_count + 1
+    x_wins = x_wins + 1
   }
   else if (win === "O") {
-    o_wins_count = o_wins_count + 1
+    o_wins = o_wins + 1
   }
-  x_wins.innerHTML = x_wins_count
-  o_wins.innerHTML = o_wins_count
+  else if (win === "T") {
+    ties = ties + 1
+  }
+  x_score.innerHTML = x_wins
+  o_score.innerHTML = o_wins
+  tie_score.innerHTML = ties
   message.textContent =
     win === "T" ? "It's a tie!" : win ? `${win} wins!` : `Turn: ${turn}`;
 }
@@ -78,6 +85,7 @@ function takeTurn(e) {
       board[index] = turn;
       turn = turn === "X" ? "O" : "X";
       win = getWinner();
+
       render();
     }
   }
@@ -90,11 +98,21 @@ function getWinner() {
     if (
       board[condition[0]] &&
       board[condition[0]] === board[condition[1]] &&
-      board[condition[1]] === board[condition[2]]
-    ) {
-      winner = board[condition[0]];
+      board[condition[0]] === board[condition[2]]
+    ){
+        winner = board[condition[0]];
     }
   });
 
   return winner ? winner : board.includes("") ? null : "T";
+}
+
+function resetScoreboard() {
+    x_wins = 0;
+    o_wins = 0;
+    ties = 0;
+
+    x_score.innerHTML = x_wins
+    o_score.innerHTML = o_wins
+    tie_score.innerHTML = ties
 }
